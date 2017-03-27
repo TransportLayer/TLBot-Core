40050ff4ba3d6fdd07aa7925110e80e921a07382
TransportLayer <TransportLayerOTR@gmail.com>
2017-03-27 13:57:36 -0700

    Add logging support

--- main.py
+++ main.py
@@ -19,10 +19,38 @@
 ###############################################################################
 
 import argparse
+import logging
+
+def setup_logger(level_string, log_file):
+    numeric_level = getattr(logging, level_string.upper(), None)
+    if not isinstance(numeric_level, int):
+        raise ValueError("Invalid log level: {}".format(level_string))
+
+    verbose_formatter = logging.Formatter("[%(asctime)s] [%(name)s/%(levelname)s] %(message)s")
+    file_formatter = verbose_formatter
+    stdout_formatter = verbose_formatter if numeric_level == logging.DEBUG else logging.Formatter("[%(asctime)s] [%(levelname)s] %(message)s", "%H:%M:%S")
+
+    root_logger = logging.getLogger(__name__)
+    root_logger.setLevel(numeric_level)
+
+    file_logger = logging.FileHandler(log_file)
+    file_logger.setFormatter(file_formatter)
+    root_logger.addHandler(file_logger)
+
+    stdout_logger = logging.StreamHandler()
+    stdout_logger.setFormatter(stdout_formatter)
+    root_logger.addHandler(stdout_logger)
 
 def main():
     parser = argparse.ArgumentParser(description="TransportLayerBot", epilog="This bot has Super Tux Powers.")
+    parser.add_argument("-l", "--log", type=str, metavar="LEVEL", dest="LOG_LEVEL", help="log level", action="store", default="INFO", required=False)
+    parser.add_argument("-o", "--output", type=str, metavar="FILE", dest="LOG_FILE", help="log file", action="store", default="TransportLayerBot.log", required=False)
     SETTINGS = vars(parser.parse_args())
+
+    setup_logger(SETTINGS["LOG_LEVEL"], SETTINGS["LOG_FILE"])
+
+    logger = logging.getLogger(__name__)
+    logger.info("Starting.")
 
 if __name__ == "__main__":
     main()
