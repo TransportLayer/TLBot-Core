280a6b7217d1909e87c17fbdd67d19e67a4d5424
TransportLayer <TransportLayerOTR@gmail.com>
2018-01-24 21:38:34 -0800

    Update copyright year

--- tlbot/client.py
+++ tlbot/client.py
@@ -1,6 +1,6 @@
 ###############################################################################
 #   TransportLayerBot: Client - All-in-one modular bot for Discord            #
-#   Copyright (C) 2017  TransportLayer                                        #
+#   Copyright (C) 2017, 2018  TransportLayer                                  #
 #                                                                             #
 #   This program is free software: you can redistribute it and/or modify      #
 #   it under the terms of the GNU Affero General Public License as published  #
